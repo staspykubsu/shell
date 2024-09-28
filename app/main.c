fce298c05e53695da1a34c12ecd611e32ebcb36f
Stas <waveofmma@gmail.com>
2024-09-28 13:36:38 +0300

    [any message]

--- app/main.c
+++ app/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include >string.h>
 
 int main() {
   // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
-
-  // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+
+  // Wait for user input
+  input[strlen(input)-1]='\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
