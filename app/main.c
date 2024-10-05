02786ca93c8ce248c1ae651d0c824c87c8597bba
Stas Pyatunin <waveofmma@gmail.com>
2024-10-05 18:17:50 +0300

    done with second point

--- app/main.c
+++ app/main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-  // Uncomment this block to pass the first stage
-  printf("$ ");
-  fflush(stdout);
-  char input[100];
-  fgets(input, 100, stdin);
-
-  // Wait for user input
-  input[strlen(input)-1]='\0';
-  printf("%s: command not found\n", input);
+  char input[1024];
+  while (1) 
+  {
+    if (fgets(input, 1024, stdin) == NULL) 
+        break;
+    printf("%s", input);
+  }
   return 0;
 }
 
