5b93d495bbf4ccc5bf066e3561c6fb7818d85677
Stas Pyatunin <waveofmma@gmail.com>
2024-10-05 18:37:20 +0300

    done with step 3 - add exit command (exit and \q)

--- app/main.c
+++ app/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   char input[1024];
   while (1) 
   {
-    if (fgets(input, 1024, stdin) == NULL) 
+    if (fgets(input, 1024, stdin) == NULL)
+        break;
+    if (strcmp(input, "exit\n") == 0 || strcmp(input, "\\q\n") == 0)
         break;
     printf("%s", input);
   }
